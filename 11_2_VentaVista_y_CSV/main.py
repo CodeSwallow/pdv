708379a24ed42d9d33de1724f48ae5cda8153f8e
CodeSwallow <86127102+CodeSwallow@users.noreply.github.com>
2021-11-08 12:16:58 -0600

    Create main.py

---
+++ 11_2_VentaVista_y_CSV/main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+from sqlqueries import QueriesSQLite
+from signin.signin import SigninWindow
+from admin.admin import AdminWindow
+from ventas.ventas import VentasWindow
+
+# agregado queriessqlite.create_tables()
+class MainWindow(BoxLayout):
+	QueriesSQLite.create_tables()
+	def __init__(self, **kwargs):
+		super().__init__(*kwargs)
+		self.admin_widget=AdminWindow()
+		self.ventas_widget=VentasWindow(self.admin_widget.actualizar_productos)
+		self.signin_widget=SigninWindow(self.ventas_widget.poner_usuario)
+		self.ids.scrn_signin.add_widget(self.signin_widget)
+		self.ids.scrn_ventas.add_widget(self.ventas_widget)
+		self.ids.scrn_admin.add_widget(self.admin_widget)
+
+class MainApp(App):
+	def build(self):
+		return MainWindow()
+
+if __name__=="__main__":
+	MainApp().run()
