46286b564951185989b4f79c37e7c88d084ba525
CodeSwallow <86127102+CodeSwallow@users.noreply.github.com>
2022-01-13 23:22:08 -0600

    Update signin.py

--- 11_2_VentaVista_y_CSV/signin/signin.py
+++ 11_2_VentaVista_y_CSV/signin/signin.py
@@ -14,11 +14,11 @@ class SigninWindow(BoxLayout):
 	def verificar_usuario(self, username, password):
 		connection = QueriesSQLite.create_connection("pdvDB.sqlite")
 		users=QueriesSQLite.execute_read_query(connection, "SELECT * from usuarios")
-		if username=='' or password=='':
-			self.ids.signin_notificacion.text='Falta nombre de usuario y/o contraseña'
-		else:
-			usuario={}
-			if users: # agregado!!!
+		if users:
+			if username=='' or password=='':
+				self.ids.signin_notificacion.text='Falta nombre de usuario y/o contraseña'
+			else:
+				usuario={}
 				for user in users:
 					if user[0]==username:
 						usuario['nombre']=user[1]
@@ -26,20 +26,25 @@ class SigninWindow(BoxLayout):
 						usuario['password']=user[2]
 						usuario['tipo']=user[3]
 						break
-			if usuario:
-				if usuario['password']==password:
-					self.ids.username.text=''
-					self.ids.password.text=''
-					self.ids.signin_notificacion.text=''
-					if usuario['tipo']=='trabajador':
-						self.parent.parent.current='scrn_ventas'
+				if usuario:
+					if usuario['password']==password:
+						self.ids.username.text=''
+						self.ids.password.text=''
+						self.ids.signin_notificacion.text=''
+						if usuario['tipo']=='trabajador':
+							self.parent.parent.current='scrn_ventas'
+						else:
+							self.parent.parent.current='scrn_admin'
+						self.poner_usuario(usuario)
 					else:
-						self.parent.parent.current='scrn_admin'
-					self.poner_usuario(usuario)
+						self.ids.signin_notificacion.text='Usuario o contraseña incorrecta'
 				else:
 					self.ids.signin_notificacion.text='Usuario o contraseña incorrecta'
-			else:
-				self.ids.signin_notificacion.text='Usuario o contraseña incorrecta'
+		else:
+		    usuario_tuple=('usuario', 'Usuario Inicio', '123', 'admin')
+		    crear_usuario = "INSERT INTO usuarios (username, nombre, password, tipo) VALUES (?,?,?,?);"
+		    QueriesSQLite.execute_query(connection, crear_usuario, usuario_tuple)
+		    self.ids.signin_notificacion.text='Se creo primer usuario. usuario 123'
 
 
 
